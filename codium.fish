11fc37ddebf24892a92c57f201270461006ba414
Jordan Williams <jordan@jwillikers.com>
2021-06-29 08:48:17 -0500

    Don't close the current terminal session when running codium

--- codium.fish
+++ codium.fish
@@ -1,11 +1,20 @@
-function codium --wraps='flatpak run com.vscodium.codium' --description 'alias the VSCodium Flatpak as codium'
+# Will still need to pass "--wait" to make it behave nicely.
+# In order to get --wait to work in a Flatpak, run
+# `flatpak override --user --env=TMPDIR=/var/tmp com.vscodium.codium`.
+# Defined via `source`
+function codium --wraps='flatpak run com.vscodium.codium' --description 'alias codium=flatpak run com.vscodium.codium'
     if test -f /app/bin/codium
-        exec /app/bin/codium $argv
+        /app/bin/codium $argv &>/dev/null &; disown
     else if test -f /usr/bin/codium
-        exec /usr/bin/codium $argv
+        /usr/bin/codium $argv &>/dev/null &; disown
     else if test -f /usr/bin/flatpak-spawn
-        exec /usr/bin/flatpak-spawn --host flatpak run com.vscodium.codium $argv
+        /usr/bin/flatpak-spawn --host flatpak run com.vscodium.codium $argv &>/dev/null &; disown
+    else if test -f /usr/bin/flatpak
+        /usr/bin/flatpak run com.vscodium.codium $argv &>/dev/null &; disown
     else
-        exec /usr/bin/flatpak run com.vscodium.codium $argv
+        if set -l index (contains -i -- --wait $argv)
+            set -e argv[$index]
+        end
+        exec vi $argv
     end
 end
